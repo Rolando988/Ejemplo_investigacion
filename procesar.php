a5f5c380e51698df5f5001ad42b171ba24f00953
Rolando988 <68891894+Rolando988@users.noreply.github.com>
2021-02-16 15:27:42 -0600

    pro

--- procesar.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-Hola <?php echo $_POST["nombre"]; ?><br>
-Tu numero de Telefono es:  <?php echo $_POST["telefono"]; ?><br>
-Tu fecha de nacimiento es:  <?php echo $_POST["fecha"]; ?><br>
-Tu email es: <?php echo $_POST["correo"]; ?>
-</body>
-</html>
